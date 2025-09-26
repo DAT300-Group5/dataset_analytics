86330569be313f31bfb9ec51480e6f88e7dc1080
Xuanhao <shirou-emiya2002@outlook.com>
2025-09-26 20:33:49 +0800

    Update: Q1 chDB

--- benchmark/queries/Q1/Q1_clickhouse.sql
+++ benchmark/queries/Q1/Q1_clickhouse.sql
@@ -1,4 +1,16 @@
 WITH
+minutes AS (
+  SELECT deviceId, toStartOfMinute(toDateTime(ts/1000, 'UTC')) AS minute_ts FROM hrm
+  UNION DISTINCT
+  SELECT deviceId, toStartOfMinute(toDateTime(ts/1000, 'UTC')) FROM ppg
+  UNION DISTINCT
+  SELECT deviceId, toStartOfMinute(toDateTime(ts/1000, 'UTC')) FROM acc
+  UNION DISTINCT
+  SELECT deviceId, toStartOfMinute(toDateTime(ts/1000, 'UTC')) FROM ped
+  UNION DISTINCT
+  SELECT deviceId, toStartOfMinute(toDateTime(ts/1000, 'UTC')) FROM lit
+),
+
 hrm_minute AS (
   SELECT
     deviceId,
@@ -32,44 +44,27 @@ ped_minute AS (
   GROUP BY deviceId, minute_ts
 ),
 
-/* --- Interpolated median (even-n -> average of the two middle values) --- */
 lit_minute AS (
   SELECT
     deviceId,
-    minute_ts,
-    /* build sorted array per (deviceId, minute_ts) */
-    if(length(vals) % 2 = 1,
-       vals[(length(vals)+1)/2],
-       (vals[length(vals)/2] + vals[length(vals)/2 + 1]) / 2.0
-    ) AS median_light
-  FROM (
-    SELECT
-      deviceId,
-      toStartOfMinute(toDateTime(ts/1000, 'UTC')) AS minute_ts,
-      arraySort(groupArray(ambient_light_intensity)) AS vals
-    FROM lit
-    GROUP BY deviceId, minute_ts
-  )
-),
-
-minutes AS (
-  SELECT deviceId, minute_ts FROM hrm_minute
-  UNION DISTINCT
-  SELECT deviceId, minute_ts FROM ppg_minute
-  UNION DISTINCT
-  SELECT deviceId, minute_ts FROM acc_minute
-  UNION DISTINCT
-  SELECT deviceId, minute_ts FROM ped_minute
-  UNION DISTINCT
-  SELECT deviceId, minute_ts FROM lit_minute
+    toStartOfMinute(toDateTime(ts/1000, 'UTC')) AS minute_ts,
+    quantileExactInclusive(0.5)(ambient_light_intensity) AS median_light
+  FROM lit
+  GROUP BY deviceId, minute_ts
 )
+
 SELECT
-  m.deviceId, m.minute_ts,
-  h.avg_hr, p.avg_ppg, a.rms_acc, d.total_steps, l.median_light
-FROM minutes m
-LEFT JOIN hrm_minute h USING (deviceId, minute_ts)
-LEFT JOIN ppg_minute p USING (deviceId, minute_ts)
-LEFT JOIN acc_minute a USING (deviceId, minute_ts)
-LEFT JOIN ped_minute d USING (deviceId, minute_ts)
-LEFT JOIN lit_minute l USING (deviceId, minute_ts)
+  m.deviceId,
+  m.minute_ts,
+  h.avg_hr,
+  p.avg_ppg,
+  a.rms_acc,
+  d.total_steps,
+  l.median_light
+FROM minutes AS m
+LEFT JOIN hrm_minute AS h USING (deviceId, minute_ts)
+LEFT JOIN ppg_minute AS p USING (deviceId, minute_ts)
+LEFT JOIN acc_minute AS a USING (deviceId, minute_ts)
+LEFT JOIN ped_minute AS d USING (deviceId, minute_ts)
+LEFT JOIN lit_minute AS l USING (deviceId, minute_ts)
 ORDER BY m.deviceId, m.minute_ts;
