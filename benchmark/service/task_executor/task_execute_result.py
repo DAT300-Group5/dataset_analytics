f599cac5b8c135ee4a8e750a5746ce77552c8798
xiejiangzhao <xieji@chalmers.se>
2025-10-10 22:05:00 +0200

    MOD: benchmark

---
+++ benchmark/service/task_executor/task_execute_result.py
@@ -0,0 +1,12 @@
+import dataclasses
+
+
+@dataclasses
+class TaskExecuteResult:
+    cpu_peek_percent: float
+    cpu_avg_percent: float
+    cpu_samples_count: int
+    cpu_sampling_interval: float
+    peak_memory_bytes: float
+    execution_time: float
+    output_rows: int
