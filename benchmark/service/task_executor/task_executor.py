9f3bcd377a81f283320ef39134deb2bbfcffa0a0
xiejiangzhao <xieji@chalmers.se>
2025-10-11 11:09:32 +0200

    MOD: benchmark

--- benchmark/service/task_executor/task_executor.py
+++ benchmark/service/task_executor/task_executor.py
@@ -13,16 +13,30 @@ class TaskExecutor:
         self.log_parser = log_parser
         self.repeat = repeat
 
-    def execute(self):
+    def execute(self) -> TaskExecuteResult:
         clean_path(self.runner.cwd / "results")
+        results = []
         for i in range(self.repeat):
             print(f"--- Execution round {i + 1} ---")
             process = self.runner.run_subprocess()
             monitor_result = monitor_subprocess(process)
             query_metric = self.log_parser.parse_log()
             task_execute_result = self.combine_results(monitor_result, query_metric)
-            print(task_execute_result)
-        return
+            print(f"--- Execution round {i + 1} results: {task_execute_result} ---")
+            results.append(task_execute_result)
+        
+        # Calculate averages across all runs
+        avg_result = TaskExecuteResult(
+            cpu_peek_percent=sum(r.cpu_peek_percent for r in results) / len(results),
+            cpu_avg_percent=sum(r.cpu_avg_percent for r in results) / len(results),
+            cpu_samples_count= int(sum(r.cpu_samples_count for r in results) / len(results)),
+            cpu_sampling_interval=sum(r.cpu_sampling_interval for r in results) / len(results),
+            peak_memory_bytes=sum(r.peak_memory_bytes for r in results) / len(results),
+            execution_time=sum(r.execution_time for r in results) / len(results),
+            output_rows=results[0].output_rows if results else 0,  # output_rows should be same across runs
+        )
+        print(f"--- Execution final averaged results: {avg_result} ---")
+        return avg_result
 
     def combine_results(self, monitor_result : ProcessMonitorResult, query_metric : QueryMetrics) -> TaskExecuteResult:
         return TaskExecuteResult(
@@ -42,5 +56,5 @@ if __name__ == "__main__":
     cwd = "/Users/xiejiangzhao/PycharmProject/dataset_analytics/benchmark/test"
     runner = SQLiteRunner(sql_file=sql_file, db_file=db_file, cmd=sqlite_cmd, cwd=cwd)
     sqlite_parser = SqliteLogParser(log_path=runner.results_dir)
-    task_executor = TaskExecutor(runner=runner, log_parser=sqlite_parser, repeat=1)
+    task_executor = TaskExecutor(runner=runner, log_parser=sqlite_parser, repeat=3)
     task_executor.execute()
