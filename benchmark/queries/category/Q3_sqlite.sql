3dee1a6d070e209e844d9c9ef833b5949b01b6c2
brisleagu <gusbrisle@student.gu.se>
2025-10-27 20:45:58 +0100

    fixing time limit unix epoch Q3_sqlite.sql

--- benchmark/queries/category/Q3_sqlite.sql
+++ benchmark/queries/category/Q3_sqlite.sql
@@ -10,7 +10,7 @@ SELECT
     (CAST(ts AS INTEGER) / 300000) * 300000 AS time_interval,  
     AVG(x*x + y*y + z*z) AS gyr_magnitude
 FROM gyr
-WHERE ts BETWEEN 1615680000 AND 1616371199
+WHERE ts BETWEEN 1615676400000 AND 1616367599000
 GROUP BY time_interval),
 
 ACC_intervals AS (
@@ -18,7 +18,7 @@ SELECT
     (CAST(ts AS INTEGER) / 300000) * 300000 AS time_interval,  
     AVG(x*x + y*y + z*z) AS acc_magnitude
 FROM acc
-WHERE ts BETWEEN 1615680000 AND 1616371199
+WHERE ts BETWEEN 1615676400000 AND 1616367599000
 GROUP BY time_interval
 )
 
@@ -42,5 +42,5 @@ END AS type_of_activity
 FROM HR_intervals h
 JOIN ACC_intervals a ON h.time_interval = a.time_interval
 JOIN GYR_intervals g ON h.time_interval = g.time_interval
-WHERE h.time_interval BETWEEN 1615680000 AND 1616371199
+WHERE h.time_interval BETWEEN 1615676400000 AND 1616367599000
 ORDER BY h.time_interval;
